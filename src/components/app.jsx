29813da15041f82d696d19e848786963780a954f
kevinraynaud <kraynaud@gmail.com>
2020-08-13 12:59:21 +0200

    app finished

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import ChannelList from '../containers/channel_list';
+import MessageList from '../containers/message_list';
 
 const App = () => {
   return (
-    <div className="app">
-      <p>React + Redux starter</p>
+    <div className="messaging-wrapper">
+      <div className="logo-container">
+        <img className="messaging-logo" src="https://dwj199mwkel52.cloudfront.net/assets/lewagon-logo-square-fe76916e1b923ade71e253ae6dc031d936e5e8eebac4e26b0fbac650ea6ee360.png" alt=""/>
+      </div>
+      <ChannelList />
+      <MessageList />
     </div>
   );
 };
